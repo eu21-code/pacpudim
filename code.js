a910be5c27bc5b2d5066bf8534048bb3d9e080ee
eu21 <85627786+eu21-code@users.noreply.github.com>
2022-05-03 15:12:22 -0300

    Update code.js

--- code.js
+++ code.js
@@ -1,8 +1,6 @@
 var p5Inst = new p5(null, 'sketch');
-
 window.preload = function () {
   initMobileControls(p5Inst);
-
   p5Inst._predefinedSpriteAnimations = {};
   p5Inst._pauseSpriteAnimationsByDefault = false;
   var animationListJSON = {"orderedKeys":["38071b7c-ce01-4ecf-8957-cabe568b2d21","a23eae7b-d9a0-4d16-96c4-6400768c4b92"],"propsByKey":{"38071b7c-ce01-4ecf-8957-cabe568b2d21":{"sourceSize":{"x":128,"y":128},"frameSize":{"x":128,"y":128},"frameCount":1,"frameDelay":4,"name":"pudim","sourceUrl":"assets/v3/animations/NCq67KXHpWX2gT86K7qrU226kOL1GMM7u5asitRqj4E/38071b7c-ce01-4ecf-8957-cabe568b2d21.png","size":7053,"version":"JagXoqpRYCMmxyYoYu87fW4Mi.3w8aEx","looping":true,"loadedFromSource":true,"saved":true,"rootRelativePath":"assets/v3/animations/NCq67KXHpWX2gT86K7qrU226kOL1GMM7u5asitRqj4E/38071b7c-ce01-4ecf-8957-cabe568b2d21.png"},"a23eae7b-d9a0-4d16-96c4-6400768c4b92":{"sourceSize":{"x":128,"y":128},"frameSize":{"x":128,"y":128},"frameCount":1,"frameDelay":4,"name":"colher","sourceUrl":"assets/v3/animations/NCq67KXHpWX2gT86K7qrU226kOL1GMM7u5asitRqj4E/a23eae7b-d9a0-4d16-96c4-6400768c4b92.png","size":5564,"version":"40AuZnjJNddbXTi6L39qscwPzE1DnHQ9","looping":true,"loadedFromSource":true,"saved":true,"rootRelativePath":"assets/v3/animations/NCq67KXHpWX2gT86K7qrU226kOL1GMM7u5asitRqj4E/a23eae7b-d9a0-4d16-96c4-6400768c4b92.png"}}};
@@ -23,7 +21,6 @@ window.preload = function () {
       p5Inst._predefinedSpriteAnimations[props.name].frameDelay = props.frameDelay;
     });
   });
-
   function wrappedExportedCode(stage) {
     if (stage === 'preload') {
       if (setup !== window.setup) {
@@ -33,15 +30,12 @@ window.preload = function () {
       }
     }
 // -----
-
 var pudim = createSprite(200,325,10,10);
 pudim.setAnimation("pudim");
 pudim.scale = 0.3;
-
 var colher = createSprite(200,70,10,10);
 colher.setAnimation("colher");
 colher.scale = 0.3;
-
 var paredes = createGroup();
 var parede1 = createSprite(50,180,100,5);
 var parede2 = createSprite(50,220,100,5);
@@ -57,7 +51,6 @@ var parede11 = createSprite(200,200,5,100);
 var parede12 = createSprite(200,200,100,5);
 var parede13 = createSprite(200,100,100,5);
 var parede14 = createSprite(200,300,100,5);
-
 paredes.add(parede1);
 paredes.add(parede2);
 paredes.add(parede3);
@@ -72,57 +65,42 @@ paredes.add(parede11);
 paredes.add(parede12);
 paredes.add(parede13);
 paredes.add(parede14);
-
-var tp1 = createSprite(1,200,1,100);
-var tp2 = createSprite(399,200,1,100);
-tp1.shapeColor = "black";
-tp2.shapeColor = "black";
-
 var estado="inicio";
-
 var pontos=0;
-
 function draw() {
   background("black");
-  
   pudim.bounce(paredes);
   colher.bounce(paredes);
-
   pudim.setCollider("circle",0,0,50);
   colher.setCollider("circle",0, 0, 50);
-  // colher.debug = true;
-  // pudim.debug = true;
-  
-  
   fill("white");
   text("github.com/eu21-code",150,15);
-  
-  if(pudim.isTouching(tp1)){
-    pudim.x = 380;
+  if(pudim.x <= 20){
+    pudim.x = 350;
     pudim.y = 200;
   }
-  if(pudim.isTouching(tp2)){
-    pudim.x = 20;
+  if(pudim.x >= 380){
+    pudim.x = 50;
     pudim.y = 200;
   }
-  if(colher.isTouching(tp1)){
-    colher.x = 380;
+  if(colher.x <= 20){
+    colher.x = 350;
     colher.y = 200;
   }
-  if(colher.isTouching(tp2)){
-    colher.x = 20;
+  if(colher.x >= 380){
+    colher.x = 50;
     colher.y = 200;
   }
   if(estado==="inicio"){
     fill("white");
     text("Press 'Space' to play", 155, 270);
     text("Press 'p' to pause", 165, 290);
-    
+    text("a w s d", 180,40);
+    text("⬅️ ⬆️ ⬇️ ➡️", 165, 365);
     if(keyDown("space")){
       estado="jogar";
     }
   }
-  
   if(estado==="jogar"){
     move();
   }
@@ -150,9 +128,7 @@ function draw() {
   }
   drawSprites();
 }
-
 function move(){
-  //pudim
   if(keyDown("up")){
     pudim.y -=4;
   }
@@ -165,7 +141,6 @@ function move(){
   if(keyDown("right")){
     pudim.x +=4;
   }
-  //colher
   if(keyDown("w")){
     colher.y -=4;
   }
@@ -193,7 +168,6 @@ function move(){
   window.wrappedExportedCode = wrappedExportedCode;
   wrappedExportedCode('preload');
 };
-
 window.setup = function () {
   window.wrappedExportedCode('setup');
 };
